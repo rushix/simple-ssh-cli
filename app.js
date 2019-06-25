c3ecef2a317fbd3ab2bee742cfb9d20626b10ac1
rushix <alter.rushi@gmail.com>
2019-06-25 10:45:57 +0300

    Simple ssh client implementation

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Simple ssh client');
+'use strict';
+
+const SshClient = require('./lib/SshClient');
+const Terminal = require('./lib/Terminal');
+
+(async function() {
+  const ssh = new SshClient({ applicationDir: __dirname });
+  const rl = new Terminal();
+
+  await ssh.connect();
+
+  try {
+    while (ssh.hasConnection) {
+      const command = await rl.readLine();
+      await ssh.executeCommand(command);
+    }
+  } catch(error) {
+    console.log(`Runtime error: ${ JSON.stringify(error) }`);
+  }
+
+})();
